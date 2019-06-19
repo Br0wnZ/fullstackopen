2fc2f8204b8d168025b9890b87a3c378d7b65ae5
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2019-06-19 10:51:25 +0200

    Part1 finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({ header }) => { return (<> <h1>{ header }</h1></>)}
+
+const Button = ({ handlerClick, text }) => { return (<button onClick={ handlerClick }>{ text }</button>)}
+
+const Statics = ({ text }) => { return (<h3>{ text }</h3>) }
+
+const Item = ({ text, count }) => { return (<p>{ text } { count }</p>) }
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const header = 'give feedback'
+  const statics = 'statics'
+  const states = ['good', 'neutral', 'bad']
+
+  const addGood = value  => () => setGood(value) 
+  const addNeutral = value => () =>  setNeutral(value) 
+  const addBad = value => ()  => setBad(value) 
+
   return (
     <div>
-      code here
+      <Header header={ header } />
+      <Button handlerClick={ addGood(good+1) } text={ states[0] }/>
+      <Button handlerClick={addNeutral(neutral+1) } text={ states[1] }/>
+      <Button handlerClick={ addBad(bad+1) } text={ states[2] }/>
+      <Statics text={ statics }/>
+      <Item text={ states[0] } count={good}/>
+      <Item text={ states[1] } count={neutral}/>
+      <Item text={ states[2] } count={bad}/>
     </div>
   )
 }
